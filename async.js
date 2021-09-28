88546faba8a297882a8e7d431240927b0699c7ac
SARA JENSEN <SSOJENSEN@GMAIL.COM>
2021-09-27 17:07:28 -0700

    adding HTML and abiltiy to draw a random card from a deck

--- async.js
+++ async.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// BASE_URL = "http://numbersapi.com";
-
 async function favoriteNum(){
     let response = await axios.get("http://numbersapi.com/9?json");
     console.log(response.data)
@@ -25,32 +23,32 @@ async function fourFacts() {
 }
 
 // Why aren't these working?
-// async function fourFactsPartTwo() {
-//     let response1 = axios.get("http://numbersapi.com/9?json");
-//     let response2 = axios.get("http://numbersapi.com/9?json");
-//     let response3 = axios.get("http://numbersapi.com/9?json");
-//     let response4 = axios.get("http://numbersapi.com/9?json");
+async function fourFactsPartTwo() {
+    let response1 = axios.get("http://numbersapi.com/9?json");
+    let response2 = axios.get("http://numbersapi.com/9?json");
+    let response3 = axios.get("http://numbersapi.com/9?json");
+    let response4 = axios.get("http://numbersapi.com/9?json");
 
-//     let results = [await response1.data, 
-//                 await response2.data, 
-//                 await response3.data, 
-//                 await response4.data];
+    let results = [await response1, 
+                await response2, 
+                await response3, 
+                await response4];
 
-//     console.log(results)
-// }
+    console.log(results)
+}
 
-// function fourFactsPartThree() {
-//     let response1 = axios.get("http://numbersapi.com/9?json");
-//     let response2 = axios.get("http://numbersapi.com/9?json");
-//     let response3 = axios.get("http://numbersapi.com/9?json");
-//     let response4 = axios.get("http://numbersapi.com/9?json");
+async function fourFactsPartThree() {
+    let response1 = axios.get("http://numbersapi.com/9?json");
+    let response2 = axios.get("http://numbersapi.com/9?json");
+    let response3 = axios.get("http://numbersapi.com/9?json");
+    let response4 = axios.get("http://numbersapi.com/9?json");
 
-//     let results = Promise.all([response1.data, 
-//                 response2.data, 
-//                 response3.data, 
-//                 response4.data]);
-//      console.log(results)
-// }
+    let results = await Promise.all([response1, 
+                response2, 
+                response3, 
+                response4]);
+     console.log(results)
+}
 
 async function shuffleDeck() {
     let cardResponse = await axios.get(`http://deckofcardsapi.com/api/deck/new/draw/?count=1`);
@@ -81,5 +79,25 @@ async function getTwoCards(){
     let card2Value = cardResponse2.data.cards[0].value
 
     console.log(`card 1 is a ${card1Value} of ${card1Suit}`, `card 2 is a ${card2Value} of ${card2Suit}`)
+}
+
+//////////////////////////////// DRAW A CARD PAGE ////////////////////
+
+let deckId = '';
+
+async function drawACard(){
+    let cardresponse =  await axios.get(`http://deckofcardsapi.com/api/deck/${deckId}/draw?count=1`)
+    let cardImage = cardresponse.data.cards[0].image;
+
+    $(".card-image").attr("src", cardImage)
 
 }
+
+async function setupPage() {
+    let deckResponse = await axios.get("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+
+    deckId = deckResponse.data.deck_id
+}
+
+$(".card-button").on("click", drawACard);
+$(document).ready(setupPage)
